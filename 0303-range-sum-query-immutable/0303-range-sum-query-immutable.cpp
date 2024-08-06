fbe85417c3081c816ab37b0acc09d3db028e522c
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-06 15:16:42 +0100

    Time: 13 ms (91.68%), Space: 22 MB (38.83%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -2,18 +2,23 @@ class NumArray {
 public:
     vector<int> prefixsum;
     NumArray(vector<int>& nums) {
-        int count = 0;
-        for(int num:nums) {
-            count = count+ num;
-            prefixsum.push_back(count);
+        prefixsum.push_back(nums[0]);
+        for(int i= 1;i< nums.size();i++) {
+            prefixsum.push_back(prefixsum.back() + nums[i]);
         }
     }
     
     int sumRange(int left, int right) {
         int ans = prefixsum[right];
-        if(left -1>=0) {
-            ans = ans-prefixsum[left-1];
+        if(left-1 >= 0){
+            ans = ans - prefixsum[left-1]; 
         }
         return ans;
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
