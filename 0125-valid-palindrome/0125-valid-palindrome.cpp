b709e21037848b5bfa7231066cb375e48852ee8a
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-06 15:23:40 +0100

    Time: 4 ms (79.51%), Space: 8.9 MB (45.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i;
-        int j;
-        for(i = 0 , j = s.size()-1; i<j;i++,j--) {
-            while(i<j && !isalnum(s[j])) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i <= j) {
+            while(i < j && !isalnum(s[j])) {
                 j--;
             }
-            while(i<j && !isalnum(s[i])) {
+            while(i < j && !isalnum(s[i])) {
                 i++;
             }
             if(tolower(s[i]) != tolower(s[j])) {
                 return false;
             }
+         i++;
+         j--;
         }
         return true;
-        
     }
 };
