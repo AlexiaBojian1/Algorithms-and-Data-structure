f5708a8005f642b6b193172be01ec42fd27cd745
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-06 11:42:16 +0100

    Time: 247 ms (5.24%), Space: 129.9 MB (100%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        if (k == 0) {
+            return nums;
+        }
+
+        int n = nums.size();
+        vector<int> result(n, -1);
+        if (2 * k + 1 > n) {
+            return result;
+        }
+        long long windowSum = 0;
+        for (int i = 0; i < (2 * k + 1); ++i) {
+            windowSum += nums[i];
+        }
+        result[k] = windowSum / (2 * k + 1);
+         for (int i = (2 * k + 1); i < n; ++i) {
+              windowSum = windowSum - nums[i - (2 * k + 1)] + nums[i];
+            result[i - k] = windowSum / (2 * k + 1);
+        }
+
+        return result;
+    }
+};
