69a81987997aa7ed30e8da4ac003cac549cd9e37
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-06 11:28:54 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        vector<int> prefix = {nums[0]};
+        int mini = nums[0];
+        for(int i=1; i < nums.size();i++) {
+            prefix.push_back(prefix.back() + nums[i]);
+            mini = min(mini, prefix.back());
+        }
+        int ans =  1 - mini;
+        return (ans != 0) ? ans : 1;
+    }
+};
