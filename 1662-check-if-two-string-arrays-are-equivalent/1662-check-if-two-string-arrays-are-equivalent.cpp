ca1d26f7b9276bd395d0ca4c67a5df06837432a8
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-16 10:31:23 +0200

    Time: 0 ms (100%), Space: 13.3 MB (89.86%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        std::string str1, str2;
+    int i1 = 0, i2 = 0; // indices for words in word1 and word2
+        int j1 = 0, j2 = 0; // indices for characters within the current word
 
-        // Concatenate all strings in word1
-        for(const std::string &s : word1) {
-            str1 += s;
+        while (i1 < word1.size() && i2 < word2.size()) {
+            // Compare the current characters
+            if (word1[i1][j1] != word2[i2][j2]) {
+                return false;
+            }
+
+            // Move to the next character in word1
+            if (++j1 == word1[i1].length()) {
+                j1 = 0;
+                i1++;
+            }
+
+            // Move to the next character in word2
+            if (++j2 == word2[i2].length()) {
+                j2 = 0;
+                i2++;
+            }
         }
 
-        // Concatenate all strings in word2
-        for(const std::string &s : word2) {
-            str2 += s;
-        }
-
-        // Compare the concatenated strings
-        return str1 == str2;
+        // Both arrays should be fully traversed if they are equal
+        return i1 == word1.size() && i2 == word2.size();
     }
 };
