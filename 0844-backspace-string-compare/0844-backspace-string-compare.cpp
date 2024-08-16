07a1c8707a56842b0ea2a7a991f2abbb41652a37
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-08-16 10:15:36 +0200

    Time: 0 ms (100%), Space: 7.8 MB (55.9%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -3,16 +3,17 @@ public:
     bool backspaceCompare(string s, string t) {
         return build(s) == build(t);
     }
-    string build(string s) {
+
+    string build (string s) {
         string ans = "";
-        for(char c : s) {
-            if(c!='#') {
-                ans.push_back(c);
-            } else if(!ans.empty())
-                {
-                    ans.pop_back(); 
-            }
-        }
+        for(char c: s){
+                if(c != '#'){
+                    ans.push_back(c);
+                } else if(!ans.empty()){
+                    ans.pop_back();
+                }
+                
+        }   
         return ans;
-    }
+     }
 };
