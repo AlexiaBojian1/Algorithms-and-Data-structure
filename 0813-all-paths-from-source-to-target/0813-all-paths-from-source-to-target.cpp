eeb0e57eb34d386f74455f6f44e59060eab01a7b
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-10-19 12:44:08 +0200

    Time: 0 ms (100%), Space: 13.4 MB (63.8%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int target = int(graph.size()) - 1;
-        vector<int> path = {0};  // Start the path with node 0 (the source node)
-        vector<vector<int>> result;
-        backtrack(0, path, result, target, graph);
-        return result;
+        vector<int> path = {0};
+        int target = graph.size() - 1;
+        vector<vector<int>> results;
+        backtrack(0, target, graph, path, results);
+        return results;
     }
-    
-    void backtrack(int currnode, vector<int>& path, vector<vector<int>>& result, int target, vector<vector<int>>& graph) {
-        // If the current node is the target, we've found a valid path
-        if(currnode == target) {
-            result.push_back(path);  // Copy the current path to the result
+
+    void backtrack(int curr, int& target, vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& results) {
+        if(curr == target){
+            results.push_back(path);
             return;
         }
 
-        // Explore all next nodes connected to the current node
-        for(int nextnode : graph[currnode]) {
-            path.push_back(nextnode);            // Add the next node to the current path
-            backtrack(nextnode, path, result, target, graph); // Recurse with the next node
-            path.pop_back();                     // Backtrack by removing the last node
+        for(int nextnode: graph[curr]) {
+            path.push_back(nextnode);
+            backtrack(nextnode, target, graph, path, results);
+            path.pop_back();
         }
     }
 };
