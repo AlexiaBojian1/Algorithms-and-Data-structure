c8d011b6fe8a7aed730ee5354aaecc4b3ebf2b22
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-10-24 13:41:44 +0200

    Time: 25 ms (98.57%), Space: 88.4 MB (28.07%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -4,14 +4,15 @@ public:
         int maxlen = 0;
         int count = 0;
         unordered_map<int,int> map;
-        map[0]=-1;
-        for(int i =0;i<nums.size();i++) {
-            count = count+(nums[i]==1 ? 1:-1);
+        map[0] = -1;
+        for(int i = 0; i< nums.size();i++) {
+            count += (nums[i] == 1 ? 1:-1);
             if(map.find(count) != map.end()) {
-                maxlen = max(maxlen, i- map[count]);
-            } else{
+                maxlen = max(maxlen, i-map[count]);
+            } else {
                 map[count] = i;
             }
+            
         }
         return maxlen;
     }
