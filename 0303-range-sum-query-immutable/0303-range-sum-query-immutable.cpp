ddf7b5cd2ed50dcb2ed76985b40ee50175ce1a44
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-10-31 12:32:27 +0100

    Time: 4 ms (43.21%), Space: 22.2 MB (22.37%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -2,16 +2,17 @@ class NumArray {
 public:
     vector<int> prefixsum;
     NumArray(vector<int>& nums) {
-        prefixsum.push_back(nums[0]);
-        for(int i= 1;i< nums.size();i++) {
-            prefixsum.push_back(prefixsum.back() + nums[i]);
+        int count = 0;
+        for(int num:nums) {
+            count = count+ num;
+            prefixsum.push_back(count);
         }
     }
     
     int sumRange(int left, int right) {
         int ans = prefixsum[right];
-        if(left-1 >= 0){
-            ans = ans - prefixsum[left-1]; 
+        if(left -1>=0) {
+            ans = ans-prefixsum[left-1];
         }
         return ans;
     }
