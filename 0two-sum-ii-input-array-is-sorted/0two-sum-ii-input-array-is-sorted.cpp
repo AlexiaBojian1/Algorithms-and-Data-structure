6e946878da79bdbfeb7ed48bb17cc1624083412b
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-11-01 11:08:43 +0100

    Time: 3 ms, Memory: 18.2 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0;
+        int high = numbers.size() - 1;
+        vector<int> ans;
+        while(low < high) {
+            if(numbers[low] + numbers[high] ==  target){
+                ans.push_back(low+1);
+                ans.push_back(high+1);
+                break;
+            } 
+            if(numbers[low] + numbers[high] < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return ans;
+    }
+};
