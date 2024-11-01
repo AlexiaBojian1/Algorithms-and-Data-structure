e7044bb8fa68cd1a8ff3bb483e5c0a476ec43207
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-11-01 17:07:13 +0100

    Time: 0 ms (100%), Space: 18.3 MB (29.37%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        stack<int> stack;
+        int j=0;
+        for(int x:pushed) {
+            stack.push(x);
+            while(!stack.empty() && j<n && stack.top() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return j == n;
+    }
+};
