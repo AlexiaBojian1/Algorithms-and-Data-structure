0504f035d8732764a94d36fcedfb01ee13326b25
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-11-04 10:25:09 +0100

    Time: 0 ms (100%), Space: 11.2 MB (74.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,10 +6,10 @@ public:
         }
 
         for(int i = 0;i < strs[0].size(); i++) {
-            chat c =  strs[0][i];
+            char c =  strs[0][i];
             for(int j = 1; j < strs.size();j++) {
                 if(i == strs[j].size() || c != strs[j][i]){
-                    return strs[0].substring(0,i);
+                    return strs[0].substr(0,i);
                 } 
             }
         }
