3badf2b645ee345e584c48c52be9cfeffb36f43a
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-11-30 18:09:33 +0100

    Time: 12 ms (93.33%), Space: 322.8 MB (52.68%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int pos = 0;
+        int n = nums.size();
+        vector<int> sweep(n + 1, 0);
+        int csum = 0;
+
+        for(int i = 0; i < n; i++) {
+            csum += sweep[i];
+            sweep[i] = 0;
+
+            if(csum < nums[i]) {
+                if(pos == queries.size()) return -1;
+                
+                int l = queries[pos][0];
+                int r = queries[pos][1];
+                int val = queries[pos][2];
+
+                if(r >= i) {
+                    if(l <=i && r >= i) csum += val;
+                    else sweep[l] += val;
+
+                    sweep[r+1] -= val;
+                }
+                pos++;
+                i--;
+            }
+        }
+        return pos;
+    }
+};
