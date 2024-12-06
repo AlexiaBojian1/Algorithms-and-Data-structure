484bea79862c59aa3001e5bdaaea78a24dd3e61b
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2024-12-06 16:07:45 +0100

    Time: 0 ms (100.00%) | Memory: 15.9 MB (6.56%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -4,17 +4,18 @@ public:
     unordered_set<int> seen;
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         this->rooms = rooms;
+        int n = rooms.size();
         seen.insert(0);
         dfs(0);
-        return rooms.size()== seen.size();
+        return rooms.size() == seen.size();
     }
-    void dfs(int node) {
-        for( int nid : rooms[node]) {
-            if(seen.find(nid) == seen.end()){
-                 seen.insert(nid);
+
+    void dfs(int room) {
+        for (int nid : rooms[room]) {
+            if (seen.find(nid) == seen.end()) {
+                seen.insert(nid);
                 dfs(nid);
             }
-              
-            }
         }
+    }
 };
