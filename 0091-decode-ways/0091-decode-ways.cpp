1b0feff59208cc13c3eb8c93186b2daf826c8966
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2025-02-01 15:53:37 +0100

    Time: 0 ms (100%), Space: 8.9 MB (48.08%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = (s[0] == '0') ? 0 : 1;
+        int pprev = 1;
+        int prev = (s[0] == '0') ? 0 : 1;
+        int current = 0;
         for(int i = 2; i <= n; i++) {
+            current = 0;
             int onedigit = stoi(s.substr(i - 1, 1));
             if(onedigit >= 1 && onedigit <= 9) {
-                dp[i] += dp[i-1];
+                current += prev;
             }
 
             int twodigit = stoi(s.substr(i - 2, 2));
             if(twodigit >= 10 && twodigit <= 26) {
-                dp[i] += dp[i - 2];
+                current += pprev;
             }
+            pprev = prev;
+            prev = current;
         }
-        return dp[n];
+        return prev;
     }
 };
