6fd5ce40b9af4020074e1a4bceb3448d347f838c
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2025-02-04 07:15:19 +0100

    Time: 0 ms (100%), Space: 9.8 MB (95.08%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
         vector<vector<int>> ans;
-        vector<int> curr = {};
-        backtrack(curr, 0, ans, nums);
+        backtrack(0, curr, ans, nums);
         return ans;
     }
 
-    void backtrack( vector<int>& curr, int i,  vector<vector<int>>& ans,  vector<int>& nums ) {
+    void backtrack( int i, vector<int> & curr, vector<vector<int>> & ans, vector<int> & nums) {
         if(i > nums.size()) {
             return;
         }
 
         ans.push_back(curr);
-        for(int j = i;j< nums.size();j++) {
+        for(int j = i; j < nums.size(); j++) {
             curr.push_back(nums[j]);
-            backtrack(curr,j+1, ans, nums);
+            backtrack(j + 1, curr, ans, nums);
             curr.pop_back();
         }
     }
