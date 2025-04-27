bdb80721b6a0f824b3c6a9d931e2c87f07649185
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2025-04-27 11:59:37 +0200

    Time: 54 ms (14.29%), Space: 56.7 MB (90.45%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size(); // rows
+        int n = boxGrid[0].size(); // columns
+        vector<vector<char>> result(n, vector<char>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = boxGrid[j][i];
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            reverse(result[i].begin(), result[i].end());
+        }
+
+        for(int j = 0; j < m ; j++) {
+            for(int i = n - 1; i >= 0; i--) {
+                if(result[i][j] == '.') {
+                    int next = -1;
+                    for(int k = i - 1; k >=0 ; k--) {
+                        if(result[k][j] == '*'){
+                            break;
+                        }
+                        if(result[k][j] == '#') {
+                            next = k;
+                            break;
+                        }
+                    }
+                    if(next != -1) {
+                        result[next][j] = '.';
+                        result[i][j] = '#';
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
