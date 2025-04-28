adda276426203fee391628ddf51a5f5f4f0127b6
AlexiaBojian1 <132288126+AlexiaBojian1@users.noreply.github.com>
2025-04-28 16:27:51 +0200

    Time: 56 ms (45.18%), Space: 51.1 MB (12.29%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> counts;
-        counts[0] = 1;
-        int ans = 0;
+        unordered_map<int,int> freq;
+        freq[0] = 1;
         int curr = 0;
-        for(int num:nums) {
-            curr = curr+num;
-            ans = ans + counts[curr-k];
-            counts[curr]++;
+        int res = 0;
+        for(auto num : nums) {
+            curr += num;
+            res = res + freq[curr - k];
+            freq[curr]++;
         }
-        return ans;
+        return res;
     }
 };
