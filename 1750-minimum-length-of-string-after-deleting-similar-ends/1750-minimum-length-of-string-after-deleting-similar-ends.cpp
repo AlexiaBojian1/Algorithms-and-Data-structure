802f7314284cfb18f88a3984bfef912556e7dca7
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-05-19 17:03:59 +0200

    Time: 0 ms (100%), Space: 15.8 MB (91.34%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -5,18 +5,16 @@ public:
         int i = 0;
         int j = n - 1;
 
-        while (i < j && s[i] == s[j]) {
-            char currentChar = s[i];
-            // Move i to the right past all characters equal to s[i]
-            while (i <= j && s[i] == currentChar) {
+        while(i < j && s[i] == s[j]) {
+            char curr = s[i];
+            while(i <= j && curr == s[i]) {
                 i++;
             }
-            // Move j to the left past all characters equal to s[j]
-            while (i <= j && s[j] == currentChar) {
+
+            while(i <= j && curr == s[j]){
                 j--;
             }
         }
-        // Return the remaining length
         return j - i + 1;
     }
 };
