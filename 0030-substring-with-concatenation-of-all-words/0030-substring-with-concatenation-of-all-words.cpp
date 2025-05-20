86049e39d00472cf60549f4bff33c7af7c35a297
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-05-20 11:20:21 +0200

    Time: 21 ms (72.54%), Space: 24 MB (45.09%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,64 +1,57 @@
 class Solution {
-private:
-    unordered_map<string, int> wordCount;
+public:
     int n;
-    int wordLength;
-    int substringSize;
     int k;
-    void slidingWindow(int left, string &s, vector<int> &answer) {
-        unordered_map<string, int> wordsFound;
-        int wordsUsed = 0;
+    int wordLength;
+    unordered_map<string, int> wordCount;
+    int substringSize;
+    void slidingWindow( int left, string &s, vector<int> &answer) {
         bool excessWord = false;
-        for (int right = left; right <= n - wordLength; right += wordLength) {
+        unordered_map<string, int> wordFound;
+        int wordUsed = 0;
+        for(int right = left ; right <= n - wordLength; right += wordLength) {
             string sub = s.substr(right, wordLength);
-            if (!wordCount.count(sub)) {
-                // Mismatched word - reset the window
-                wordsFound.clear();
-                wordsUsed = 0;
+            if(!wordCount.count(sub)) {
+                wordFound.clear();
                 excessWord = false;
+                wordUsed = 0;
                 left = right + wordLength;
             } else {
-                // If we reached max window size or have an excess word
-                while (right - left == substringSize || excessWord) {
-                    string leftmostWord = s.substr(left, wordLength);
+                while(right - left == substringSize || excessWord) {
+                    string leftMost = s.substr(left, wordLength);
                     left += wordLength;
-                    wordsFound[leftmostWord]--;
-                    if (wordsFound[leftmostWord] >= wordCount[leftmostWord]) {
-                        // This word was an excess word
+                    wordFound[leftMost] --;
+                    if(wordFound[leftMost] >= wordCount[leftMost]) {
                         excessWord = false;
                     } else {
-                        // Otherwise we actually needed it
-                        wordsUsed--;
+                        wordUsed --;
                     }
                 }
-                // Keep track of how many times this word occurs in the window
-                wordsFound[sub]++;
-                if (wordsFound[sub] <= wordCount[sub]) {
-                    wordsUsed++;
-                } else {
-                    // Found too many instances already
+
+                wordFound[sub]++;
+                if(wordFound[sub] > wordCount[sub]) {
                     excessWord = true;
+                } else {
+                    wordUsed ++;
                 }
-                if (wordsUsed == k && !excessWord) {
-                    // Found a valid substring
+                if(wordUsed == k && !excessWord ) {
                     answer.push_back(left);
                 }
             }
         }
     }
-
-public:
-    vector<int> findSubstring(string s, vector<string> &words) {
+    vector<int> findSubstring(string s, vector<string>& words) {
         n = s.length();
+        wordLength = words[0].size();
         k = words.size();
-        wordLength = words[0].length();
         substringSize = wordLength * k;
-        for (string &word : words) {
+        for(string &word : words) {
             wordCount[word]++;
         }
+
         vector<int> answer;
-        for (int i = 0; i < wordLength; i++) {
-            slidingWindow(i, s, answer);
+        for(int i = 0; i < wordLength ; i++) {
+            slidingWindow(i , s, answer);
         }
         return answer;
     }
