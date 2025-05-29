11ea66d47fd7ac80015dc3a0239cd177831ea638
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-05-30 00:09:29 +0200

    Time: 24 ms (79.22%), Space: 17 MB (54.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    int m ;
-    int n;
-    vector<pair<int,int>> directions=  {{0,1}, {1,0}, {-1,0}, {0,-1}};
-    vector<vector<bool>> seen;
     vector<vector<char>> grid;
-    int numIslands(vector<vector<char>>& grid) {
+    vector<vector<bool>> seen;
+    int m;
+    int n;
+    vector<pair<int,int>>  directions=  {{0,1}, {1,0}, {-1,0}, {0,-1}};
 
+    int numIslands(vector<vector<char>>& grid) {
         this->grid = grid;
         n = grid.size();
-         m = grid[0].size();
+        m = grid[0].size();
         seen = vector(n, vector<bool>(m, false));
 
         int ans = 0;
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m ;j++) {
-                if(!seen[i][j] && grid[i][j] == '1') {
+            for(int j = 0; j < m;j++) {
+                if(!seen[i][j] && grid[i][j] == '1'){
                     ans++;
                     seen[i][j] = true;
                     dfs(i,j);
                 }
             }
         }
-
         return ans;
     }
 
-    void dfs(int row, int col) {
-        for( auto direction : directions) {
-            int newrow = row + direction.first;
-            int newcol = col + direction.second;
-            if(valid(newrow, newcol) && !seen[newrow][newcol]) {
-                seen[newrow][newcol] = true;
-                dfs(newrow, newcol);
+    void dfs(int i, int j) {
+        for(auto direction : directions) {
+            int newi = i + direction.first;
+            int newj = j + direction.second;
+            if(valid(newi , newj) && !seen[newi][newj]) {
+                seen[newi][newj] = true;
+                dfs(newi, newj);
             }
         }
     }
 
     bool valid(int i, int j) {
-        return 0<= i && i < n && 0 <= j && j < m && grid[i][j] == '1';
+        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1';
     }
 };
