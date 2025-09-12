da52629879b9499cfa81a0205e92da90d13e98b4
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-12 14:14:18 +0200

    Time: 9 ms (39.52%), Space: 11.6 MB (64.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -3,27 +3,28 @@ public:
     vector<int> findAnagrams(string s, string p) {
         int np = p.length();
         int ns = s.length();
+        vector <int> result;
 
         if(ns < np) return {};
 
-        std::vector<int> pCount(26, 0), sCount(26, 0);
-        vector<int> output;
+        vector<int> pcount(26, 0), scount(26, 0);
+
         for(char ch : p) {
-            pCount[ch - 'a']++;
+            pcount[ch - 'a']++;
         }
 
-        for(int i = 0;i < ns; i++) {
-            sCount[s[i] - 'a']++;
+        for(int i = 0; i < ns; i++) {
+
+            scount[s[i] - 'a']++;
 
             if(i >= np) {
-                sCount[s[i - np] - 'a']--;
-            }
+                scount[s[i - np] - 'a']--;
+            } 
 
-            if(pCount == sCount) {
-                output.push_back(i-np+1);
+            if(pcount == scount) {
+                result.push_back(i - np + 1);
             }
         }
-
-        return output;
+        return result;
     }
 };
