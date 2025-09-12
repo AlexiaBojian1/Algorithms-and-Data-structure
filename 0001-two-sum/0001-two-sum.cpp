2d830749129330cfc5250ef943abf1f324ae1458
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-12 10:53:48 +0200

    Time: 11 ms (42.38%), Space: 15.9 MB (6.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> map;
+        vector<int> result;
+        unordered_map<int, int> map;
         for(int i = 0; i < nums.size(); i++) {
-            int compliment = target - nums[i];
-            if(map.find(compliment) != map.end()) {
-                return {map[compliment] , i};
+            int compliement  = target - nums[i];
+            if(map.find(compliement) != map.end()) {
+                result.push_back(i);
+                result.push_back(map[compliement]);
             }
             map[nums[i]] = i;
         }
-        return {0,0};
+        return result;
     }
 };
