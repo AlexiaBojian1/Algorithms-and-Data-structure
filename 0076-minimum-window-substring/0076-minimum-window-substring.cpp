406f6b4dbb212bf7e6e991e92d283a3206ed1a57
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-12 16:59:13 +0200

    Time: 19 ms (21.48%), Space: 11.6 MB (43.62%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.length() == 0 || s.length() == 0) {
-            return "";
-        }
-
-        unordered_map<char , int> freq;
-        for(char c : t) {
-            freq[c]++;
-        }
-
-        int required = freq.size();
-        int left = 0;
         int right = 0;
         int formed = 0;
-        unordered_map<char , int> windowCount;
-        int ans[3] = {-1, 0, 0}; // size, left, right
+        int left = 0;
+        int ns = s.length();
+        int nt = t.length();
+        if(ns < nt) {
+            return "";
+        }
+        unordered_map<char, int> freq;
+        for(char ch : t) freq[ch] ++;
+        int requiered = freq.size();
+        unordered_map<char, int> window_count;
+        int ans[3] = {-1, 0, 0};
+
         while(right < s.length()) {
-            char c = s[right];
-            windowCount[c] ++;
-            if(freq.find(c) != freq.end() && windowCount[c] == freq[c]) {
+            window_count[s[right]]++;
+            if(freq.find(s[right]) != freq.end() && window_count[s[right]] == freq[s[right]]) {
                 formed++;
             }
 
-            while(left <= right && formed == required) {
-                c = s[left];
-                if(ans[0] == -1 || right - left + 1 < ans[0]) {
+            while(left <= right && formed == requiered) {
+                char c = s[left];
+                if(ans[0] > right - left + 1 || ans[0] == -1) {
                     ans[0] = right - left + 1;
                     ans[1] = left;
                     ans[2] = right;
                 }
-                windowCount[c] --;
-                if(freq.find(c) != freq.end() && windowCount[c] < freq[c]) {
+                window_count[c]--;
+                if(freq.find(c) != freq.end() && window_count[c] < freq[c]) {
                     formed --;
                 }
                 left++;
             }
             right++;
         }
+
         return ans[0] == -1 ? "" : s.substr(ans[1], ans[2] - ans[1] + 1);
     }
 };
