6c21ef546268d06d0f8377b08a82d1e8ece8d7cf
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-14 15:53:50 +0200

    Time: 10 ms (34.76%), Space: 101.6 MB (47.59%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.cpp
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int shortestWordDistance(vector<string>& wordsDict, string word1, string word2) {
-        int shortestDistance = INT_MAX;
+        int shortdist = INT_MAX;
         int prevIndex = -1;
-        for(int i = 0;i < wordsDict.size(); i++) {
+        for(int i = 0; i < wordsDict.size(); i++) {
             if(wordsDict[i] == word1 || wordsDict[i] == word2) {
-                if (prevIndex != -1 && (wordsDict[prevIndex] != wordsDict[i] || word1 == word2))
-                {
-                    shortestDistance = min(shortestDistance, i - prevIndex);
+                if(prevIndex != -1 && (wordsDict[prevIndex] != wordsDict[i] || word1 == word2)) {
+                    shortdist = min(shortdist, i - prevIndex);
                 }
                 prevIndex = i;
             }
         }
-        return shortestDistance;
+        return shortdist;
     }
 };
