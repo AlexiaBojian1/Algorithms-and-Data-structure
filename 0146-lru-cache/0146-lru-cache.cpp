d861ee1d5012444c7e9f0410956869bd78bdbe4f
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-14 13:07:38 +0200

    Time: 80 ms (59.39%), Space: 177.9 MB (52.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,29 +1,27 @@
 struct Node {
     int val;
     int key;
-    Node *next;
-    Node *prev;
+    Node* next;
+    Node* prev;
     Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
 };
 
+
 class LRUCache {
 public:
-    
     int capacity;
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
     unordered_map<int, Node*> dic;
 
     LRUCache(int capacity) {
         this->capacity = capacity;
         head->next = tail;
-        tail->prev = head;
+        tail->prev = head;    
     }
     
     int get(int key) {
-        if(dic.find(key) == dic.end()) {
-            return -1;
-        }
+        if(dic.find(key) == dic.end()) return -1;
 
         Node* node = dic[key];
         remove(node);
@@ -33,28 +31,30 @@ public:
     
     void put(int key, int value) {
         if(dic.find(key) != dic.end()) {
-            Node* oldnode = dic[key];
-            remove(oldnode);
+            Node* old = dic[key];
+            remove(old);
         }
-        Node* node = new Node(key,value);
+
+        Node* node = new Node(key, value);
         dic[key] = node;
         add(node);
-        if(dic.size() > capacity){
-            Node * nodetodelete = head->next;
-            remove(nodetodelete);
-            dic.erase(nodetodelete->key);
+
+        if(dic.size() > capacity) {
+            Node* nodeToDelete = head->next;
+            remove(nodeToDelete);
+            dic.erase(nodeToDelete->key);
         }
     }
 
-    void add(Node * node) {
-        Node * prevEnd = tail->prev;
-        prevEnd -> next = node;
-        node-> prev = prevEnd;
-        node-> next = tail;
+    void add(Node* node) {
+        Node* prevNode = tail->prev;
+        prevNode->next = node;
+        node->prev = prevNode;
+        node->next = tail;
         tail->prev = node;
     }
 
-    void remove(Node * node) {
+    void remove(Node* node) {
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
