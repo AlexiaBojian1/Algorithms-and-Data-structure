dabaf9a6991ae2932df94bf8668cce3caa3168b0
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-14 15:38:24 +0200

    Time: 39 ms (40.15%), Space: 19 MB (49.85%) - LeetHub

---
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        vector<int> a_indices;
+        vector<int> b_indices;
+        vector<int> result;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.substr(i, a.size()) == a) {
+                a_indices.push_back(i);
+            }
+        }
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s.substr(i, b.size()) == b) {
+                b_indices.push_back(i);
+            }
+        }
+
+        int j = 0;
+        for(int i : a_indices) {
+            while(j < b_indices.size() && b_indices[j] < i - k){
+                j++;
+            }
+
+            if(j < b_indices.size() && abs(b_indices[j] - i) <= k) {
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+};
