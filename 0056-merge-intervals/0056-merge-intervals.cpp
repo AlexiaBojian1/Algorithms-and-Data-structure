49e5d79bbe52cc646cf80f688fcc5f1eec077369
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-14 12:08:19 +0200

    Time: 7 ms (41.2%), Space: 24.8 MB (15.9%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin() , intervals.end());
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> merged;
         for(auto interval : intervals) {
             if(merged.empty() || merged.back()[1] < interval[0]) {
