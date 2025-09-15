5a67a7d0b650a67f1db65696e3586ee974f6aec4
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-15 17:17:01 +0200

    Time: 43 ms (83.33%), Space: 30.4 MB (66.67%) - LeetHub

---
+++ 2964-number-of-divisible-triplet-sums/2964-number-of-divisible-triplet-sums.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int divisibleTripletCount(vector<int>& nums, int d) {
+        if(nums.size() < 3) return 0;
+        int output = 0;
+        unordered_map<int, int> map;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int need = (d - nums[i] % d) % d;
+            output += map[need];
+
+            for(int j = 0; j < i; j++) {
+                map[(nums[i] + nums[j] ) % d]++;
+            }
+        }
+        return output;
+    }
+};
