058cfd34f8b79997083763fed3f9f6f19b4991ed
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-09-19 14:13:03 +0200

    Time: 1 ms (48.78%), Space: 15.3 MB (75.82%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if (prices.empty()) return 0;
-
+        if(prices.empty()) return 0;
         int n = prices.size();
         vector<int> buy(n, 0), sell(n, 0), cooldown(n, 0);
-
-        // Initial conditions
         buy[0] = -prices[0];
         sell[0] = 0;
         cooldown[0] = 0;
 
-        for (int i = 1; i < n; ++i) {
-            // Maximum profit on day `i` if we buy: either continue holding from previous buy, or cooldown and buy today
+        for(int i = 1; i < n; i++) {
             buy[i] = max(buy[i - 1], cooldown[i - 1] - prices[i]);
-            // Maximum profit on day `i` if we sell: can only sell if we bought before
             sell[i] = buy[i - 1] + prices[i];
-            // Maximum profit on day `i` if we cooldown: can either continue cooldown from previous day or transition from sell
             cooldown[i] = max(cooldown[i - 1], sell[i - 1]);
         }
 
-        // The answer will be the max profit on the last day if we either sold or were in cooldown (cannot end on a buy)
         return max(sell[n - 1], cooldown[n - 1]);
-        
     }
 };
