d2107da5586dc1837c55126175a300476cb6029e
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-11-12 14:56:35 +0100

    Time: 462 ms (12.87%), Space: 43 MB (13.15%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> res; // store unique triplets
-        unordered_set<int> dups; // keep track of duplicates
-        unordered_map<int, int> seen; // key = values of array , value = index
+        set<vector<int>> res;
+        unordered_set<int> dups;
+        unordered_map<int, int> seen;
 
-        for(int i = 0;i < nums.size(); i++) {
-            if(dups.insert(nums[i]).second)  // true is nums[i] is not in dups
-            {
-                for(int j = i+1; j < nums.size() ; j++) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(dups.insert(nums[i]).second) {
+                for(int j = i + 1; j < nums.size(); j++) {
                     int complement = -nums[i] - nums[j];
                     auto it = seen.find(complement);
                     if(it != seen.end() && it->second == i) {
                         vector<int> triplet = {nums[i], nums[j], complement};
-                        sort(begin(triplet), end(triplet));
+                        sort(triplet.begin(), triplet.end());
                         res.insert(triplet);
                     }
                     seen[nums[j]] = i;
@@ -21,6 +20,6 @@ public:
             }
         }
         return vector<vector<int>>(begin(res), end(res));
-
+        
     }
 };
