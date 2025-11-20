5729ce9555151978f00aa0231d59977914fc1c9b
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2025-11-20 10:44:03 +0100

    Time: 51 ms (63.53%), Space: 43.1 MB (55.14%) - LeetHub

---
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        vector<double> medians;
+        multiset<int> window(nums.begin(), nums.begin() + k);
+
+        auto mid = next(window.begin(), k/2);
+
+        for(int i = k;; i++) {
+            medians.push_back(((double)(*mid) + *next(mid, k % 2 - 1)) * 0.5);
+
+            if(i == nums.size()) {
+                break;
+            }
+
+            window.insert(nums[i]);
+            if(nums[i] < *mid) {
+                mid--;
+            }
+
+            if(nums[i - k] <= *mid) {
+                mid++;
+            }
+
+            window.erase(window.lower_bound(nums[i - k]));
+        }
+
+        return medians;
+    }
+};
