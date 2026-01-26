580f857c923597dff27451f51dd3ce5e83a27d32
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2026-01-26 13:35:30 +0100

    Time: 4 ms (40.01%), Space: 29.6 MB (29.32%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> grid;
-    vector<vector<int>> directions = {{1,0}, {0,1} , {-1,0},{0,-1}};
-    int m;
-    int n;
     vector<vector<bool>> seen;
+    int n;
+    int m;
+    vector<pair<int, int>> directions = {{0 , 1}, {1 , 0}, {0 , -1}, {-1 , 0}};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int current = 0;
-        int maxarea = 0;
         this->grid = grid;
         n = grid.size();
         m = grid[0].size();
-        seen = vector(n, vector<bool>(m , false));
-
+        seen = vector(n, vector(m , false));
+        int maxarea = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(!seen[i][j] && grid[i][j] == 1) {
+                if(grid[i][j] == 1 && !seen[i][j]) {
                     seen[i][j] = true;
-                    current = dfs(i,j);
-                    maxarea = max(maxarea, current);
+                    maxarea = max(maxarea, dfs(i, j));
+                    
                 }
             }
         }
         return maxarea;
     }
 
-    int dfs(int i, int j) {
+    int dfs(int x, int y) {
         int area = 1;
-        for(auto direction : directions) {
-            int newi = direction[0] + i;
-            int newj = direction[1] + j;
-            if( valid(newi, newj) && !seen[newi][newj]) {
-                seen[newi][newj] = true;
-                 area += dfs(newi, newj);
+        for(auto dir : directions) {
+            int newx = x + dir.first;
+            int newy = y + dir.second;
+            if(valid(newx, newy) && !seen[newx][newy]) {
+                seen[newx][newy] = true;
+                area += dfs(newx, newy);
             }
         }
         return area;
     }
-     bool valid(int i, int j) {
-        return 0<=i && i <n && 0 <=j && j < m && grid[i][j] == 1;
+
+    bool valid(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1;
     }
 };
