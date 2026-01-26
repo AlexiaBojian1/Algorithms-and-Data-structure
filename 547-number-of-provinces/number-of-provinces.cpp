84b2aa7f1a5355951341777ea90ddb6f27c0dfbc
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2026-01-26 12:53:44 +0100

    Time: 1 ms (48.76%) | Memory: 20.3 MB (41.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -4,32 +4,32 @@ public:
     vector<bool> seen;
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        int m = isConnected[0].size();
         seen = vector(n, false);
-        for(int i = 0; i < n ;i++) {
-            for(int j = i + 1; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < m; j++) {
                 if(isConnected[i][j] == 1) {
                     graph[i].push_back(j);
                     graph[j].push_back(i);
                 }
             }
         }
-
         int ans = 0;
         for(int i = 0; i < n; i++) {
             if(!seen[i]) {
-                ans++;
+                ans ++;
                 seen[i] = true;
                 dfs(i);
             }
         }
         return ans;
     }
-    
+
     void dfs(int node) {
-        for(int neightbour : graph[node]) {
-            if(!seen[neightbour]) {
-                seen[neightbour] = true;
-                dfs(neightbour);
+        for(auto neightbours : graph[node]) {
+            if(!seen[neightbours]) {
+                seen[neightbours] = true;
+                dfs(neightbours);
             }
         }
     }
