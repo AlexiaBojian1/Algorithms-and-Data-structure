73285849122131f11e38401028b6a4ce8358f86f
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2026-01-26 12:53:40 +0100

    Time: 1 ms (48.76%), Space: 20.3 MB (41.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<bool> seen;
     unordered_map<int, vector<int>> graph;
+    vector<bool> seen;
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        seen = vector(n , false);
-        for(int i = 0; i < n ;i++) {
-            for(int j = i + 1; j < n ; j++) {
+        int m = isConnected[0].size();
+        seen = vector(n, false);
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < m; j++) {
                 if(isConnected[i][j] == 1) {
                     graph[i].push_back(j);
                     graph[j].push_back(i);
                 }
             }
         }
-
         int ans = 0;
-        for(int i = 0; i < n ; i++) {
+        for(int i = 0; i < n; i++) {
             if(!seen[i]) {
-                ans++;
+                ans ++;
                 seen[i] = true;
                 dfs(i);
             }
@@ -26,10 +26,10 @@ public:
     }
 
     void dfs(int node) {
-        for(int neigh : graph[node]) {
-            if(!seen[neigh]) {
-                seen[neigh] = true;
-                dfs(neigh);
+        for(auto neightbours : graph[node]) {
+            if(!seen[neightbours]) {
+                seen[neightbours] = true;
+                dfs(neightbours);
             }
         }
     }
