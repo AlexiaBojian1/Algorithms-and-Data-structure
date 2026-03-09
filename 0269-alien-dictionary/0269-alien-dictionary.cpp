3cd93b935a3aa2dee67aabdce888c2cb2d00ad8a
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2026-03-09 14:27:17 +0100

    Time: 4 ms (17.82%), Space: 12.6 MB (21.8%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        unordered_map<char, unordered_set<char>> graph;
-        unordered_map<char,int> inDegree;
+      unordered_map<char, unordered_set<char>> graph;
+      unordered_map<char, int> degree;
 
-        for(const string& word: words) {
-            for (char c : word) {
-                graph[c];
-                inDegree[c] = 0;
-            }
+      for(auto word : words) {
+        for(auto c: word) {
+            graph[c];
+            degree[c] = 0;
         }
+      }
 
-        for(int i = 0; i < words.size() - 1; i++) {
-            string& w1 = words[i];
-            string& w2 = words[i + 1];
-            int minLen = min(w1.size(), w2.size());
-            bool foundDiff = false;
-
-            for(int j = 0; j < minLen; j++) {
-                char c1 = w1[j];
-                char c2 = w2[j];
-                if(c1 != c2) {
-                    if(!graph[c1].count(c2)) {
-                        graph[c1].insert(c2);
-                        inDegree[c2]++;
-                    }
-                    foundDiff = true;
-                    break;
+      for(int i =0; i < words.size() - 1; i++) {
+        string& w1 = words[i];
+        string& w2 = words[i + 1];
+        bool foundiff = false;
+        int minlen = min(w1.size(), w2.size());
+        for(int j = 0; j < minlen; j++) {
+            char c1 = w1[j];
+            char c2 = w2[j];
+            if(c1 != c2) {
+                if(!graph[c1].count(c2)) {
+                    graph[c1].insert(c2);
+                    degree[c2]++;
                 }
-            }
-            if (!foundDiff && w1.size() > w2.size()) return "";
-        }
-
-        queue<char> q;
-        for(auto& [c, deg] : inDegree) {
-            if(deg == 0) q.push(c);
-        }
-
-        string result;
-        while(!q.empty()) {
-            char curr = q.front();
-            q.pop();
-            result += curr;
-            for(char neighbor : graph[curr]) {
-                if(--inDegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
+                foundiff = true;
+                break;
             }
         }
-        if(result.size() != graph.size()) return "";
-        return result;
+        if (!foundiff && w1.size() > w2.size()) return "";
+      }  
+      queue<char> q;
+      for(auto [c, deg] : degree) {
+        if(deg == 0) q.push(c);
+      }
+      
+      string result;
+      while(!q.empty()) {
+        char current = q.front();
+        q.pop();
+        result += current;
+        for(auto nei : graph[current]) {
+            degree[nei]--;
+            if(degree[nei] == 0) {
+                q.push(nei);
+            }
+        }
+      }
+      return result.size() == graph.size() ? result : "";
     }
 };
