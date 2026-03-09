79bf819865018d107e43335b6090527817ed113c
Alexia Bojian <132288126+AlexiaBojian1@users.noreply.github.com>
2026-03-09 10:54:59 +0100

    Time: 26 ms (63.31%) | Memory: 17.2 MB (52.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    vector<vector<char>> grid;
-    int m;
-    int n;
-    vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<pair<int, int>> directions = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
     vector<vector<bool>> seen;
+    vector<vector<char>> grid;
+    int n;
+    int m;
     int numIslands(vector<vector<char>>& grid) {
         this->grid = grid;
         n = grid.size();
         m = grid[0].size();
-        seen = vector(n, vector(m , false));
         int ans = 0;
+        seen = vector(n, vector(m , false));
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == '1' && !seen[i][j]) {
@@ -23,36 +23,18 @@ public:
         return ans;
     }
 
-    // void dfs(int x, int y) {
-    //     for(auto dir : directions) {
-    //         int newx = x + dir.first;
-    //         int newy = y + dir.second;
-    //         if(valid(newx, newy) && !seen[newx][newy]) {
-    //             seen[newx][newy] = true;
-    //             dfs(newx, newy);
-    //         }
-    //     }
-    // }
-
-    void dfs(int x, int y) {
-        stack<pair<int, int>> stack;
-        stack.push(pair(x, y));
-
-        while(!stack.empty()) {
-            auto[row, col] = stack.top();
-            stack.pop();
-            for(auto dir : directions) {
-                int newx = row + dir.first;
-                int newy = col + dir.second;
-                if(valid(newx, newy) && !seen[newx][newy]) {
-                    seen[newx][newy] = true;
-                    stack.push(pair(newx, newy));
-                }
+    void dfs(int row, int col) {
+        for(auto dir : directions) {
+            int newrow = dir.first + row;
+            int newcol = dir.second + col;
+            if(valid(newrow, newcol)) {
+                seen[newrow][newcol] = true;
+                dfs(newrow, newcol);
             }
         }
     }
 
-    bool valid(int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == '1';
+    bool valid(int row, int col) {
+        return row >= 0 && col >= 0 && row < n && col < m && !seen[row][col] && grid[row][col] == '1';
     }
 };
